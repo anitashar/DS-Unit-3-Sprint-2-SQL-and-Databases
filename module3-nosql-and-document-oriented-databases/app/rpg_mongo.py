96ec32ff1c16b68566f7a4db4a3afc27067cfb56
anitashar <anitashar2006@gmail.com>
2020-04-18 13:55:09 -0700

    updated

--- module3-nosql-and-document-oriented-databases/app/rpg_mongo.py
+++ module3-nosql-and-document-oriented-databases/app/rpg_mongo.py
@@ -27,6 +27,8 @@ DB_USER = os.getenv("MONGO_USER", default="OOPS")
 DB_PASSWORD = os.getenv("MONGO_PASSWORD", default="OOPS")
 CLUSTER_NAME = os.getenv("MONGO_CLUSTER_NAME", default="OOPS")
 
+print(CLUSTER_NAME)
+
 #Read the db file
 DB_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "data", "rpg_db.sqlite3")
 connection = sqlite3.connect(DB_FILEPATH)
@@ -37,8 +39,10 @@ print(type(cursor)) #> <class 'sqlite3.Cursor'>
 
 character = cursor.execute("SELECT * FROM charactercreator_character").fetchall()
 
+
+
 #Connect to a MOngo DB database 
-connection_uri = "mongodb+srv://{DB_USER}:{DB_PASSWORD}@{CLUSTER_NAME}.mongodb.net/test?retryWrites=true&w=majority"
+connection_uri = f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@{CLUSTER_NAME}.mongodb.net/test?retryWrites=true&w=majority"
 print("----------------")
 print("URI:", connection_uri)
 
@@ -50,12 +54,26 @@ cursor = connection.cursor()
 print(type(cursor)) #>
 
 
-db = client.character # "rpg_database" or whatever you want to call it
+db = client.rpg_mongo # "rpg_database" or whatever you want to call it
 print("----------------")
 print("DB:", type(db), db)
 
-collection = db.rpg_mongo # "rpg_mongo" or whatever you want to call it
+collection = db.character # "rpg_mongo" or whatever you want to call it
 print("----------------")
 print("COLLECTION:", type(collection), collection)
 
+# inserting data into table--here character is in line 40
 
+for rw in character:
+    collection.insert_one({
+    "character_id": rw[0],
+    "name":rw[1] ,
+    "level":rw[2], 
+    "exp":rw[3], 
+    "hp":rw[4], 
+    "strength":rw[5], 
+    "intelligence":rw[6], 
+    "dexterity":rw[7], 
+    "wisdom":rw[8]
+        
+    })
